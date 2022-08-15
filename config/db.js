041c1f8f6ab7a1ef7f95364c85815c06b1e8c03e
Emivw <emiya.vanwyk@gmail.com>
2022-08-15 11:46:06 +0200

    first commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 require('dotenv').config;
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.dbhost,
     user: process.env.dbuser,
-    database: process.env.db,
+    database: process.env.database,
     password: process.env.dbpass,
-    port: process.env.dbport
+    port: process.env.dbport,
+    connectionLimit: 10
 });
-connection.connect();
+// connection.connect();
 module.exports = connection;
