390556a5f1e91ff3abbede92b61072826bb12d4c
Emivw <emiya.vanwyk@gmail.com>
2022-08-16 13:58:01 +0200

    first commit

--- config/db.js
+++ config/db.js
@@ -1,26 +1,39 @@
+// require('dotenv').config();
+// const { createConnection } = require('mysql');
+// // Create connection variable
+// let connection;
+// // Problem solved
+// (function handleConnection() {
+//     connection = createConnection({
+//         host: process.env.dbHost,
+//         user: process.env.dbUser,
+//         password: process.env.dbPassword,
+//         port: process.env.dbPort,
+//         database: process.env.database,
+//         multipleStatements: true
+//     });
+//     connection.connect((err) => {
+//         if (err) throw err
+//     });
+//     connection.on('error', (err) => {
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             handleConnection();
+//         } else {
+//             throw err;
+//         }
+//     })
+// })();
+// module.exports = connection;
+
 require('dotenv').config();
-const { createConnection } = require('mysql');
-// Create connection variable
-let connection;
-// Problem solved
-(function handleConnection() {
-    connection = createConnection({
-        host: process.env.dbHost,
-        user: process.env.dbUser,
-        password: process.env.dbPassword,
-        port: process.env.dbPort,
-        database: process.env.database,
-        multipleStatements: true
-    });
-    connection.connect((err) => {
-        if (err) throw err
-    });
-    connection.on('error', (err) => {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleConnection();
-        } else {
-            throw err;
-        }
-    })
-})();
+const { createPool } = require('mysql');
+const connection = createPool({
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPassword,
+    port: process.env.dbPort,
+    database: process.env.database,
+    multipleStatements: true,
+    connectionLimit: 10
+});
 module.exports = connection;
